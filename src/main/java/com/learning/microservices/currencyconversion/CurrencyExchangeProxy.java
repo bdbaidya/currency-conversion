53fc48346d00a3b27f32f4824b858bbfb0ed0edf
Debashis B <debashis.baidya@kiwigrid.com>
2024-03-25 22:20:28 +0100

    Feign request implemented

---
+++ src/main/java/com/learning/microservices/currencyconversion/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.learning.microservices.currencyconversion;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{fromcurr}/to/{tocurr}")
+	public CurrencyConversion retriveExchangeValue(
+			@PathVariable String fromcurr,
+			@PathVariable String tocurr );
+
+}
