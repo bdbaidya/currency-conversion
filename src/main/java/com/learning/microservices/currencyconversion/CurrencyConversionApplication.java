652ab7224011c2cac6d7db2f90545850a8b21074
Debashis B <debashis.baidya@kiwigrid.com>
2024-04-05 02:13:59 +0200

    Deployment branch initated.

--- src/main/java/com/learning/microservices/currencyconversion/CurrencyConversionApplication.java
+++ src/main/java/com/learning/microservices/currencyconversion/CurrencyConversionApplication.java
@@ -3,9 +3,11 @@ package com.learning.microservices.currencyconversion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableZipkinServer
 public class CurrencyConversionApplication {
 
 	public static void main(String[] args) {
